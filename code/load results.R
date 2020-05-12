fbf2c6cdef240ec4902ad92922039227ae386061
Francisco <fhenaod@zoology.ubc.ca>
2020-05-12 12:21:38 -0700

    + best model selection code improved

--- code/load results.R
+++ code/load results.R
@@ -20,14 +20,15 @@ for(i in 1:length(models)){
 }
 
 mod_comp <- data.frame(models = models[1:length(mar_Lik)], mar_Lik)
-mod_comp$BF <-round(abs(2*(mod_comp$mar_Lik[which(max(mod_comp$mar_Lik)==mar_Lik)]-mod_comp$mar_Lik)), 2)
-mod_comp[which(max(mod_comp$BF)==mod_comp$BF),] # Best model
-best_model <- mod_comp[which(max(mod_comp$BF)==mod_comp$BF),][1,1]
+mod_comp$BF <-round(abs(2*(mod_comp$mar_Lik[which(max(mod_comp$mar_Lik, na.rm = T)==mar_Lik)]-mod_comp$mar_Lik)), 2)
+mod_comp[which(max(mod_comp$BF, na.rm = T)==mod_comp$BF),] # Best model
+mod_comp %>% arrange((BF))
+best_model <- mod_comp[which(max(mod_comp$BF, na.rm = T)==mod_comp$BF),][1,1]
 #
 
 chain.best <- readRDS(paste0(path, best_model, "/chain.", best_model, ".rds"))
 chain.best <- set.burnin(chain.best, 0.3)
-sum_best <- summary(chain.best)
+#sum_best <- summary(chain.best)
 sum_best <- readRDS(paste0(path, best_model, "/sum_", best_model, ".rds"))
 sum_best$statistics
 head(sum_best$branch.posteriors)
@@ -52,6 +53,7 @@ dev.off()
 
 plotBranchHeatMap(d_fruit_lg_ln$phy, chain.best, "alpha", pal = cm.colors, cex = .1)
 plotBranchHeatMap(d_fruit_lg_ln$phy, chain.best, "beta_newguinea", pal = cm.colors, cex = .1)
+plotBranchHeatMap(d_fruit_lg_ln$phy, chain.best, "theta", pal = cm.colors, cex = .1)
 
 # l1ou ####
 library(dplyr)
